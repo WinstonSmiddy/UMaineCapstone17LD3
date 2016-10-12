ac584647acbf05dc6555d8769c9ee43ce8d27014
Gregory Smiddy <gregory.smiddy@gmail.com>
2016-10-12 17:25:10 -0400

    whole lotta stuff

--- src/LD3_ToyCar.ino
+++ src/LD3_ToyCar.ino
@@ -19,13 +19,9 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #include <Adafruit_GPS.h>
 #include <SoftwareSerial.h>
-
 #include <Wire.h>
 #include <LSM303.h>
 #include <RunningMedian.h>
-
-// #include <SPI.h>
-// #include <SD.h>
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //  PREPROCESSOR DEFINITIONS
@@ -35,36 +31,26 @@
 #define DEBUG_DEC_PRECISION 4
 #define DEBUG_GPS_RAW  false
 #define DEBUG_MOTORS true
-#define LOOP_DELAY 100
 
-#define CONST_BAUD_GPS 9600 // Could use 4800 if needed, but 9600 is default
+#define CONST_LOOP_DELAY 100
+#define CONST_BAUD_GPS 9600
 #define CONST_BAUD_SERIAL 115200
 #define CONST_FILTER_SIZE 5 // 1 to 19
 #define CONST_PI 3.14159265
 #define CONST_G 9.805
-
-// #define CONST_xRawMin -16384
-// #define CONST_xRawMax 16384
-// #define CONST_yRawMin -16384
-// #define CONST_yRawMax 16384
-// #define CONST_zRawMin -16384
-// #define CONST_zRawMax 16384
 #define CONST_RAD_TO_DEG 57.2958
 #define CONST_FULL_TURN_RATE_DEGS 360.0
 #define CONST_NUM_WPTS 3
 #define CONST_TOL_HDG 3.0
-#define CONST_TOL_LOC     0.00002
+#define CONST_TOL_LOC 0.00001
 
 #define ABS 120 // PWM value for motors (constant for now)
-
 #define in1 3
 #define in2 4
 #define in3 5
 #define in4 6
 #define ENA 2
 #define ENB 7
-
-// #define PIN_SD_RW 10
 #define PIN_ENGINE_SWITCH 13
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -84,9 +70,6 @@ SoftwareSerial mySerial(8, 7); // MAKE SURE THE SWITCH ON THE GPS SHIELD IS SET 
 Adafruit_GPS   GPS(&mySerial);
 
 LSM303 compass;
-LSM303::vector<float> mag = { 0.0f, 0.0f, 0.0f };
-
-//  accel_G = { 0.0f, 0.0f, 0.0f };
 
 Coord loc;
 Coord wpt[CONST_NUM_WPTS] { { 44.894862, -68.659313 },
@@ -94,27 +77,19 @@ Coord wpt[CONST_NUM_WPTS] { { 44.894862, -68.659313 },
                             { 44.895003, -68.659327 } };
 Coord target = wpt[0];
 
-// RunningMedian arr_lat_raw = RunningMedian(CONST_FILTER_SIZE);
-// RunningMedian arr_lon_raw = RunningMedian(CONST_FILTER_SIZE);
-// RunningMedian arr_mx_raw  = RunningMedian(CONST_FILTER_SIZE);
-// RunningMedian arr_my_raw  = RunningMedian(CONST_FILTER_SIZE);
-
 uint32_t timer   = millis(); // Global millisecond timer
 int motorState   = 0;        // Tracks the state of the motor (or rather, the motor's commanded state)
 int motorCommand = 0;
 int nextWpt      = 1;
 
 
-boolean flag_useMotors      = true;
-boolean flag_haveGPS        = false;
-boolean flag_onFirstLoop    = true;
-boolean flag_waypointChange = false;
+boolean flag_useMotors = true;
+boolean flag_haveGPS   = false;
 
 float heading      = 0.0f;
 float track        = 0.0f;
 float headingError = 0.0f;
-float latError     = 0.0f;
-float lonError     = 0.0f;
+float locError     = 0.0f;
 
 float turnRate = CONST_FULL_TURN_RATE_DEGS;
 
@@ -142,8 +117,8 @@ void setup()
   GPS.begin(CONST_BAUD_GPS);
 
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_10HZ); // 1 Hz update rate
-  GPS.sendCommand(PGCMD_ANTENNA);             // Request updates on antenna status
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
+  GPS.sendCommand(PGCMD_ANTENNA);            // Request updates on antenna status
 
   mySerial.println(PMTK_Q_RELEASE);
 }
@@ -167,40 +142,46 @@ SIGNAL(TIMER0_COMPA_vect) {
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void loop()
 {
+  flag_haveGPS = true;
+
+
   flag_useMotors = digitalRead(PIN_ENGINE_SWITCH); // Check physical switch and set boolean accordingly
-  flag_haveGPS   = (GPS.fix && (GPS.satellites != 0));
-  timer          = (timer > millis()) ? millis() : timer;
 
-  getPhysicalStatus();
+  _mRight();
 
-
-  if (flag_haveGPS && !(flag_onFirstLoop))
-  {
-    if ((abs(latError)  > CONST_TOL_LOC) || (abs(lonError) > CONST_TOL_LOC))
-    {
-      if (abs(headingError) > CONST_TOL_HDG)
-      {
-        if (headingError < 0.0)
-        {
-          _mRight();
-        } else {
-          _mLeft();
-        }
-        delay(abs(headingError) / turnRate * 1000);
-      }
-      _mForward();
-    } else {
-      flag_waypointChange = true;
-    }
-  }
-
-  if (flag_waypointChange) changeWaypoint();
-
-  #ifdef DEBUG
-  doDebug();
-  #endif // ifdef DEBUG
-
-  delay(LOOP_DELAY);
+  // flag_haveGPS   = (GPS.fix && (GPS.satellites != 0));
+  // timer          = (timer > millis()) ? millis() : timer;
+  //
+  // getPhysicalStatus();
+  //
+  // if (flag_haveGPS)
+  // {
+  //   if (locError > CONST_TOL_LOC)
+  //   {
+  //     if (abs(headingError) > CONST_TOL_HDG)
+  //     {
+  //       if (headingError < 0.0)
+  //       {
+  //         _mRight();
+  //       } else {
+  //         _mLeft();
+  //       }
+  //
+  //       delay(abs(headingError) / turnRate * 1000);
+  //     }
+  //
+  //     _mForward();
+  //   }
+  //   else
+  //   {
+  //     changeWaypoint();
+  //   }
+  // }
+  //
+  //
+  // #ifdef DEBUG
+  // doDebug();
+  // #endif // ifdef DEBUG
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -211,24 +192,11 @@ void changeWaypoint()
   _mStop();
   nextWpt++;
 
-  if (nextWpt > CONST_NUM_WPTS)
-  {
-    _mStop();
-    nextWpt = CONST_NUM_WPTS;
-  }
-
-  target = wpt[nextWpt];
-
-  latError = loc.lat - target.lat;
-  lonError = loc.lon - target.lon;
-
-  flag_waypointChange = false;
+  if (nextWpt > CONST_NUM_WPTS) nextWpt = CONST_NUM_WPTS;
 }
 
 void getPhysicalStatus()
 {
-  static int arrCounter = 0;
-
   // if a sentence is received, we can check the checksum, parse it...
   if (GPS.newNMEAreceived()) {
     if (!GPS.parse(GPS.lastNMEA())) return;
@@ -236,57 +204,27 @@ void getPhysicalStatus()
 
   compass.read();
 
-  // accel_G.x = (map(compass.a.x,CONST_xRawMin,CONST_xRawMax,-1000,1000)) / 1000.0;
-  // accel_G.y = (map(compass.a.y,CONST_yRawMin,CONST_yRawMax,-1000,1000)) / 1000.0;
-  // accel_G.z = (map(compass.a.z,CONST_zRawMin,CONST_zRawMax,-1000,1000)) / 1000.0;
-
-  // arr_lat_raw.add(GPS.latitudeDegrees);
-  // arr_lon_raw.add(GPS.longitudeDegrees);
-  // arr_mx_raw.add(compass.m.x);
-  // arr_my_raw.add(compass.m.y);
-
-  // arrCounter++;
-
-  // if (arrCounter >= (CONST_FILTER_SIZE - 1))
-  // {
-  // loc.lat = arr_lat_raw.getMedian();
-  // loc.lon = arr_lon_raw.getMedian();
-  // mag.y   = arr_my_raw.getMedian();
-  // mag.x   = arr_mx_raw.getMedian();
-
   loc.lat = GPS.latitudeDegrees;
   loc.lon = GPS.longitudeDegrees;
-  mag.y   = compass.m.y;
-  mag.x   = compass.m.x;
 
-  arrCounter = 0;
+  heading = atan2(-compass.m.y,compass.m.x) * CONST_RAD_TO_DEG;
 
-  heading = atan2(-mag.y,mag.x) * CONST_RAD_TO_DEG;
+  if (heading < 0) heading += 360;
 
-  if (heading < 0)
-  {
-    heading += 360;
-  }
+  track = desiredHeading(loc,target);
 
-  if ((loc.lat != 0.0) && (loc.lon != 0.0))
-  {
-    track = desiredHeading(loc,target);
+  headingError = heading - track;
 
-    headingError = heading - track;
-
-    latError = loc.lat - target.lat;
-    lonError = loc.lon - target.lon;
-  }
-
-  // flag_onFirstLoop = false;
-  // }
+  locError =
+    sqrt(pow((loc.lat - wpt[nextWpt].lat),
+             2) + pow((loc.lon - wpt[nextWpt].lon),2));
 }
 
 void _mForward()
 {
   motorCommand = 1;
 
-  if (flag_useMotors && flag_haveGPS && !(flag_onFirstLoop))
+  if (flag_useMotors && flag_haveGPS)
   {
     digitalWrite(ENA,HIGH);
     digitalWrite(ENB,HIGH);
@@ -302,7 +240,7 @@ void _mBack()
 {
   motorCommand = 2;
 
-  if (flag_useMotors && flag_haveGPS && !(flag_onFirstLoop))
+  if (flag_useMotors && flag_haveGPS)
   {
     digitalWrite(ENA,HIGH);
     digitalWrite(ENB,HIGH);
@@ -318,7 +256,7 @@ void _mLeft()
 {
   motorCommand = 3;
 
-  if (flag_useMotors && flag_haveGPS && !(flag_onFirstLoop))
+  if (flag_useMotors && flag_haveGPS)
   {
     digitalWrite(ENA,HIGH);
     digitalWrite(ENB,HIGH);
@@ -334,7 +272,7 @@ void _mRight()
 {
   motorCommand = 4;
 
-  if (flag_useMotors && flag_haveGPS && !(flag_onFirstLoop))
+  if (flag_useMotors && flag_haveGPS)
   {
     digitalWrite(ENA,HIGH);
     digitalWrite(ENB,HIGH);
@@ -348,15 +286,15 @@ void _mRight()
 
 void _mStop()
 {
-  motorCommand = 0;
   digitalWrite(ENA,LOW);
   digitalWrite(ENB,LOW);
-  motorState = motorCommand;
+  motorState = 0;
 }
 
 float desiredHeading(Coord start,Coord end)
 {
-  float track = 180.0 - (atan((end.lon - start.lon) / (end.lat - start.lat)) *
+  float track = 180.0 - (atan((end.lon - start.lon) /
+                              (end.lat - start.lat)) *
                          CONST_RAD_TO_DEG);
 
   if (track < 0) track += 360;
@@ -374,16 +312,15 @@ void doDebug() {
     Serial.print(GPS.seconds,DEC);
 
     if (flag_haveGPS) {
-      Serial.print(" - "); Serial.print((int)GPS.satellites);
-      Serial.println(
-        " satellites connected.");
+      Serial.print(" - "); Serial.print((int)(timer / 1000));
+      Serial.print(" seconds elapsed - ");
+      Serial.print((int)GPS.satellites); Serial.println(" satellites connected.");
       Serial.print("Going to waypoint "); Serial.print(nextWpt);
       Serial.print(" of "); Serial.println(CONST_NUM_WPTS);
       Serial.print("Location: ");
       Serial.print(loc.lat,6);
       Serial.print(", "); Serial.println(loc.lon,6);
-      Serial.print("Loc Error: "); Serial.print(latError,6);
-      Serial.print(", "); Serial.println(lonError,6);
+      Serial.print("Loc Error: "); Serial.println(locError,6);
     } else Serial.println("\n!!! NO GPS FIX !!!");
     Serial.print("Heading (deg.): "); Serial.println(heading);
     Serial.print("Track (deg.): "); Serial.println(track);
